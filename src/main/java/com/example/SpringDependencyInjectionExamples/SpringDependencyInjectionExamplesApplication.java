39f9667bfd8239f1d4460703930678e9b0ffee55
Sebastian <s.schweder1998@gmail.com>
2023-02-05 17:33:36 +0100

    Created project files using Spring Initializr

---
+++ src/main/java/com/example/SpringDependencyInjectionExamples/SpringDependencyInjectionExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringDependencyInjectionExamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDependencyInjectionExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDependencyInjectionExamplesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringDependencyInjectionExamples/SpringDependencyInjectionExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringDependencyInjectionExamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDependencyInjectionExamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
